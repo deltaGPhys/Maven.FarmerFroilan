ab5ee20d609f9f5fecc0bf0b3fe228e1ed46b3d8
Christopher Santana <chris@zipcoders-MacBook-Pro-6.local>
2019-11-19 14:38:45 -0500

    second commit

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/farm/CropRow.java
@@ -0,0 +1,18 @@
+package com.zipcodewilmington.froilansfarm.farm;
+
+import com.zipcodewilmington.froilansfarm.crops.Crop;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CropRow {
+
+    @Test
+
+    public void CropRow(){
+        CropRow croprow = new CropRow();
+        Assert.assertTrue(croprow instanceof CropRow);
+
+
+    }
+
+}
--- src/test/java/com/zipcodewilmington/froilansfarm/farm/Field.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/farm/Field.java
@@ -1,4 +1,14 @@
 package com.zipcodewilmington.froilansfarm.farm;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Field {
+
+    @Test
+
+    public void FieldTest(){
+        Field field = new Field();
+        Assert.assertTrue(field instanceof Field);
+    }
 }
