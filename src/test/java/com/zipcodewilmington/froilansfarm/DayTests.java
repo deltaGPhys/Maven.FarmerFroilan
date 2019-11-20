ca4ca723517d4d2a7c2fad98859efd35c9bab58c
deltagphys <cty.jgates@gmail.com>
2019-11-20 16:11:30 -0500

    tests starts

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/DayTests.java
@@ -0,0 +1,30 @@
+package com.zipcodewilmington.froilansfarm;
+
+import com.zipcodewilmington.froilansfarm.farm.Farm;
+import com.zipcodewilmington.froilansfarm.people.Farmer;
+import com.zipcodewilmington.froilansfarm.people.Pilot;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DayTests {
+
+    private Farm farm;
+    private Farmer froilan;
+    private Pilot froilanda;
+
+    @Before
+    public void setUp() throws Exception {
+        farm = new Farm();
+        froilan = new Farmer();
+        froilanda = new Pilot();
+
+    }
+
+
+    public void dailyTasks() {
+    }
+
+    @Test
+    public void Monday() {
+    }
+}
