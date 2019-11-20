d76142c848bff3761e5a95cc65c9dcdbd0671ed5
deltagphys <cty.jgates@gmail.com>
2019-11-20 16:44:24 -0500

    .

--- src/test/java/com/zipcodewilmington/froilansfarm/DayTests.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/DayTests.java
@@ -1,9 +1,13 @@
 package com.zipcodewilmington.froilansfarm;
 
+import com.zipcodewilmington.froilansfarm.farm.ChickenCoop;
 import com.zipcodewilmington.froilansfarm.farm.Farm;
 import com.zipcodewilmington.froilansfarm.farm.Field;
+import com.zipcodewilmington.froilansfarm.farm.Stable;
 import com.zipcodewilmington.froilansfarm.people.Farmer;
 import com.zipcodewilmington.froilansfarm.people.Pilot;
+import com.zipcodewilmington.froilansfarm.vehicles.CropDuster;
+import com.zipcodewilmington.froilansfarm.vehicles.Tractor;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,8 +24,22 @@ public class DayTests {
         farm = new Farm();
         froilan = new Farmer();
         froilanda = new Pilot();
-        farm.addField(new Field());
+        farm.setField(new Field());
+        //set the rows
 
+        for (int i = 0; i<4; i++) {
+            farm.addChickenCoop(new ChickenCoop());
+        }
+
+        for (int i = 0; i<3; i++) {
+            farm.addStable(new Stable());
+        }
+
+        farm.addVehicle(new Tractor());
+        farm.addVehicle(new Tractor());
+        CropDuster cropDuster = new CropDuster();
+        froilanda.fly(cropDuster);
+        farm.addVehicle(cropDuster);
     }
 
 
