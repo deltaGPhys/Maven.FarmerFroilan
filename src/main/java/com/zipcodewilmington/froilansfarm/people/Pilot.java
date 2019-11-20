c38816788b8887db3a48b646c262ec70edeb2277
vaidehi4321 <56602405+vaidehi4321@users.noreply.github.com>
2019-11-20 15:27:59 -0500

    pilot code done

--- src/main/java/com/zipcodewilmington/froilansfarm/people/Pilot.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/people/Pilot.java
@@ -3,7 +3,23 @@ package com.zipcodewilmington.froilansfarm.people;
 import com.zipcodewilmington.froilansfarm.vehicles.Aircraft;
 
 public class Pilot extends Person {
+    private Aircraft aircraft;
+
+    public Pilot(Aircraft aircraft) {
+        this.aircraft = aircraft;
+    }
+
+    public Aircraft getAircraft() {
+        return aircraft;
+    }
+
+
+
     public void fly(Aircraft aircraft){
         //aircraft
+        this.aircraft=aircraft;
+    }
+    public void stopFlying(){
+        this.aircraft=null;
     }
 }
