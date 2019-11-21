bd67fab85ce3f3a3e8e6c926f6adad5a7582711e
Christopher Santana <chris@zipcoders-MacBook-Pro-6.local>
2019-11-21 16:24:51 -0500

    pulling to ge update on vropduster

--- src/test/java/com/zipcodewilmington/froilansfarm/vehicles/CropDuster.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/vehicles/CropDuster.java
@@ -1,4 +1,13 @@
 package com.zipcodewilmington.froilansfarm.vehicles;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CropDuster {
+
+    @Test
+    public void CropDusterTest() {
+        CropDuster cropDuster = new CropDuster();
+        Assert.assertTrue(cropDuster instanceof CropDuster);
+    }
 }
