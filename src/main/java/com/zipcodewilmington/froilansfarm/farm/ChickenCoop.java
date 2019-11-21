22bff953c59fabb34b34c30e60e92a0d64af3dd0
kendrang <kendrang@users.noreply.github.com>
2019-11-21 09:22:04 -0500

    done stable and coop tests

--- src/main/java/com/zipcodewilmington/froilansfarm/farm/ChickenCoop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/farm/ChickenCoop.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class ChickenCoop {
     //List of Chickens
-    List<Chicken> chickenList;
+    List<Chicken> chickenList = new ArrayList<>();
 
     public void add(Chicken chicken){
         chickenList.add(chicken);
