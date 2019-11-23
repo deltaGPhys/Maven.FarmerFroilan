942c82eab6bced89493f8f463816f6f84959e8b5
vaidehi4321 <56602405+vaidehi4321@users.noreply.github.com>
2019-11-23 15:17:55 -0500

    working

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/WednesdayTest.java
@@ -0,0 +1,112 @@
+package com.zipcodewilmington.froilansfarm;
+
+import com.zipcodewilmington.froilansfarm.animals.Chicken;
+import com.zipcodewilmington.froilansfarm.animals.Horse;
+import com.zipcodewilmington.froilansfarm.edibles.EarCorn;
+import com.zipcodewilmington.froilansfarm.edibles.Egg;
+import com.zipcodewilmington.froilansfarm.edibles.Tomato;
+import com.zipcodewilmington.froilansfarm.farm.*;
+import com.zipcodewilmington.froilansfarm.people.Farmer;
+import com.zipcodewilmington.froilansfarm.people.Pilot;
+import com.zipcodewilmington.froilansfarm.vehicles.CropDuster;
+import com.zipcodewilmington.froilansfarm.vehicles.Tractor;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class WednesdayTest {
+    private Farm farm;
+    private Farmer froilan;
+    private Field field;
+    private Pilot froilanda;
+    private CropDuster cropDuster;
+    private FarmHouse farmHouse;
+
+
+    @Before
+    public void setUp() throws Exception {
+        farm = new Farm();
+        froilan = new Farmer("Froilan");
+        froilanda = new Pilot("Froilanda");
+        field = new Field();
+        farm.setField(field);
+        farmHouse = new FarmHouse();
+        farm.setFarmHouse(farmHouse);
+        farmHouse.add(froilan);
+        farmHouse.add(froilanda);
+
+        for (int i = 0; i<4; i++) {
+            farm.addChickenCoop(new ChickenCoop());
+        }
+
+        int i = 0;
+        while (i < 15) {
+            farm.getCoopList().get(new Random().nextInt(3)).add(new Chicken());
+            i++;
+        }
+
+        for (i = 0; i<3; i++) {
+            farm.addStable(new Stable());
+        }
+
+        i = 0;
+        while (i < 10) {
+            farm.getStableList().get(new Random().nextInt(3)).add(new Horse());
+            i++;
+        }
+
+        farm.addVehicle(new Tractor());
+        farm.addVehicle(new Tractor());
+        cropDuster = new CropDuster();
+        froilanda.fly(cropDuster);
+        farm.addVehicle(cropDuster);
+    }
+
+    @Test
+    public void setUpTest() {
+        Assert.assertEquals("Early to bed, early to rise...", froilan.makeNoise());
+        Assert.assertEquals("Pilot goes whoosh!", froilanda.makeNoise());
+
+        Assert.assertEquals(3,farm.getVehicleList().size());
+        long numTractors = farm.getVehicleList().stream().filter(x-> x instanceof Tractor).count();
+        Assert.assertEquals(2,numTractors);
+        long numPlanes = farm.getVehicleList().stream().filter(x-> x instanceof CropDuster).count();
+        Assert.assertEquals(1,numPlanes);
+
+        Assert.assertEquals(cropDuster,froilanda.getAircraft());
+        froilanda.stopFlying();
+        Assert.assertNull(froilanda.getAircraft());
+
+        Assert.assertEquals(field, farm.getField());
+        Assert.assertEquals(farmHouse, farm.getFarmHouse());
+        Assert.assertTrue(farmHouse.getPersonList().contains(froilan));
+        Assert.assertTrue(farmHouse.getPersonList().contains(froilanda));
+
+        int horses = farm.getStableList().stream().mapToInt(x -> x.getHorseList().size()).sum();
+        Assert.assertEquals(10,horses);
+
+        int chickens = farm.getCoopList().stream().mapToInt(x -> x.getChickenList().size()).sum();
+        Assert.assertEquals(15,chickens);
+    }
+
+        @Test
+        public void wednesdayEatingTestFroilan(){
+            ArrayList<String> froilansFull = new ArrayList<>();
+            for (int i = 1 ; i <= 6 ; i++ ){
+                froilansFull.add(froilan.eat(new Egg())) ;
+            }
+            froilansFull.add(froilan.eat(new EarCorn()));
+            for (int i = 1; i <=3 ; i++){
+                froilansFull.add(froilan.eat(new Tomato()));
+            }
+            Assert.assertEquals(10, froilansFull.size());
+        }
+
+
+        }
+
+
+
